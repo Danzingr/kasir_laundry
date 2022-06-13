575c2eb26a8d85d4866101af347a2165fa5a8006
FlutLab.io <>
2022-06-13 20:20:56 +0000

    Autosave

--- lib/screen/dashboard.dart
+++ lib/screen/dashboard.dart
@@ -14,7 +14,27 @@ class _dashboardState extends State<dashboard> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home Screen'),
+        title: Text('Dashboard'),
+      ),
+      body: Stack(
+        children: [
+          Align(
+              alignment: Alignment(0, 0.9),
+              child: SizedBox(
+                  height: 50,
+                  width: 150,
+                  child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          primary: Colors.red,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          )),
+                      onPressed: () {
+                        Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => loginscreen()));
+                        var box = Hive.box('userBox').clear();
+                      },
+                      child: Text('LOG OUT'))))
+        ],
       ),
     );
   }
