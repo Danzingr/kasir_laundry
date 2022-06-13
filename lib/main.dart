a3a1632fe93dbe06e0e4bb9883a2af61da2571b0
FlutLab.io <>
2022-06-13 20:15:18 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'screen/splashscreen.dart';
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 void main() async {
